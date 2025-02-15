e7ef43c0328dce1d07a9e1d31261455fcf9ca563
imanAlmohandes <imanawni1502@gmail.com>
2025-02-15 12:21:36 -0800

    تعديلات على المشروع

--- resources/views/view1.blade.php
+++ resources/views/view1.blade.php
@@ -1,23 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-<h1> Hello, {{$name}} </h1>
+    <h1> Hello, {{ $name }} </h1>
     <form action="view1" method="post">
         @csrf
 
         <input type="text" name="name" id="name">
         <select name="" id="">
             @foreach ($lang as $key => $lan)
-                <option value="{{$key}}">{{$lan}}</option>
+                <option value="{{ $key }}">{{ $lan }}</option>
             @endforeach
         </select>
-
         <input type="submit" name="send" id="">
     </form>
 </body>
+
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,39 @@
 <?php
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/view1' , function (){
+Route::get('/view1', function () {
     $name = 'iman awni';
     $lang = [
     ];
-    return view('view1' , compact('name' , 'lang'));
+    return view('view1', compact('name', 'lang'));
 });
 
-Route::post('/view1' , function (){
-    $name = $_POST['name'] ;
+Route::post('/view1', function () {
+    $name = $_POST['name'];
     $lang = [
         '1' => 'php',
         '2' => 'java',
-        '3' => 'c++'
+        '3' => 'c++',
     ];
-    return view ('view1' , compact('name' , 'lang'));
+    return view('view1', compact('name', 'lang'));
+});
+
+Route::get('/about', function () {
+    $name = 'iman awni';
+    return view('about', compact('name'));
+});
+
+Route::get('/tasks', function () {
+    return view('tasks');
+});
+Route::post('/create', function () {
+    $task_name = $_POST['name'];
+    DB::table('tasks')->insert(['name' => $task_name]);
+    return view('tasks');
 });
